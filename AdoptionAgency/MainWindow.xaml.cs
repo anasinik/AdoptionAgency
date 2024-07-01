2f90cb2c5aa03a1e0e74cbe4c6837b0b2429682c
Ana Šinik <sinikana7@gmail.com>
2024-07-01 10:55:39 +0200

    Initial commit

---
+++ AdoptionAgency/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace AdoptionAgency
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
