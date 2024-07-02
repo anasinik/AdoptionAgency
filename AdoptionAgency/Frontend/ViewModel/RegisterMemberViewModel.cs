6f34f6fb089c06ee263265baf1e86e17cc1d622d
cofi420 <filip.vidakovic.04@gmail.com>
2024-07-02 17:56:36 +0200

    feat: Add RegisterStudentViewModel

---
+++ AdoptionAgency/Frontend/ViewModel/RegisterMemberViewModel.cs
@@ -0,0 +1,78 @@
+﻿using AdoptionAgency.Backend.Domain.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AdoptionAgency.Frontend.ViewModel
+{
+    public class RegisterMemberViewModel : ViewModelBase
+    {
+        public string FirstName
+        {
+            get => FirstName;
+            set
+            {
+                FirstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get => LastName;
+            set
+            {
+                LastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Gender Gender
+        {
+            get => Gender;
+            set
+            {
+                Gender = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get => PhoneNumber;
+            set
+            {
+                PhoneNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Email
+        {
+            get => Email;
+            set
+            {
+                Email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get => Password;
+            set
+            {
+                Password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RegisterMemberViewModel()
+        {
+            
+        }
+    }
+}
