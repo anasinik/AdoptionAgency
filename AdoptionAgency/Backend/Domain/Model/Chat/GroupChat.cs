43cc6fac0a00d08490ea32801e74b3c0811bc408
cofi420 <filip.vidakovic.04@gmail.com>
2024-07-02 12:09:49 +0200

    feat: Add chat model classes

---
+++ AdoptionAgency/Backend/Domain/Model/Chat/GroupChat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoptionAgency.Backend.Domain.Model.Chat
+{
+    public class GroupChat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public GroupChat(){}
+        public GroupChat(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ AdoptionAgency/Backend/Domain/Model/Chat/Message.cs
@@ -0,0 +1,25 @@
+﻿using AdoptionAgency.Domain.Model.Person;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoptionAgency.Backend.Domain.Model.Chat
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = default!;
+        public DateTime Timestamp { get; set; }
+        public Person Person { get; set; } = default!;
+        public Message(){}
+        public Message(int id, string text, DateTime timestamp, Person person)
+        {
+            Id = id;
+            Text = text;
+            Timestamp = timestamp;
+            Person = person;
+        }
+    }
+}
