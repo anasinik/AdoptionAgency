7d07639cdee9ad51075977c56a84f716a3741c6b
Ana Šinik <sinikana7@gmail.com>
2024-07-02 14:55:18 +0200

    feat: Add ICrudRepository

---
+++ AdoptionAgency/Backend/Domain/RepositoryInterfaces/ICrudRepository.cs
@@ -0,0 +1,12 @@
+﻿
+namespace AdoptionAgency.Backend.Domain.RepositoryInterfaces
+{
+    public interface ICrudRepository<T>
+    {
+        public T Get(int id);
+        public List<T> GetAll();
+        public int Add(T entity);
+        public void Update(T entity);
+        public int Delete(int id);
+    }
+}
