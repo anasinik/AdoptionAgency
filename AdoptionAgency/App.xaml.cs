87ebb48cf7263636eef8f34300d6295e5b32d209
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-07-02 20:25:41 +0200

    feat: add connection to database

--- AdoptionAgency/App.xaml.cs
+++ AdoptionAgency/App.xaml.cs
@@ -1,12 +1,54 @@
-﻿using System.Windows;
+﻿using AdoptionAgency.Backend.Configuration;
+using AdoptionAgency.Backend.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
 
 namespace AdoptionAgency
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        private readonly IHost _host;
 
+        public App()
+        {
+            _host = CreateHost();
+            ApplyMigrations();
+        }
+
+        private IHost CreateHost()
+        {
+            return Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+            {
+                var databaseConfig = new DatabaseConfig();
+                services.AddDbContext<DatabaseContext>(options =>
+                    options.UseNpgsql(databaseConfig.GetConnectionString()));
+
+                // NOTE: Add the following line to register a service
+                // services.AddTransient<IExampleRepositry, ExampleRepository>();
+            }).Build();
+        }
+
+        private void ApplyMigrations()
+        {
+            using (var scope = _host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+                db.Database.Migrate();
+            }
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host.Start();
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _host.StopAsync().Wait();
+            base.OnExit(e);
+        }
+    }
 }
---
+++ AdoptionAgency/Backend/Configuration/DatabaseConfig.cs
@@ -0,0 +1,27 @@
+﻿using DotNetEnv;
+
+namespace AdoptionAgency.Backend.Configuration
+{
+    public class DatabaseConfig
+    {
+        public string Host { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string DatabaseName { get; set; }
+
+        public DatabaseConfig()
+        {
+            Env.Load(Constants.DOTENV_PATH);
+            Host = Utils.GetEnv("HOST") ?? throw new Exception("Database host configuration error");
+            Username = Utils.GetEnv("USERNAME") ?? throw new Exception("Database username configuration error");
+            Password = Utils.GetEnv("PASSWORD") ?? throw new Exception("Database password configuration error");
+            DatabaseName = Utils.GetEnv("DATABASE") ?? throw new Exception("Database database configuration error");
+        }
+
+        public string GetConnectionString()
+        {
+            return $"Host={Host};Database={DatabaseName};Username={Username};Password={Password}";
+        }
+
+    }
+}
