144385996bbab074ffa4996f2f1bf300693ab505
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-07-03 18:19:18 +0200

    feat: implement AddAnimal View

---
+++ AdoptionAgency/Frontend/View/Post/AddAnimal.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace AdoptionAgency.Frontend.View.Post
+{
+    public partial class AddAnimal : UserControl
+    {
+        public AddAnimal()
+        {
+            InitializeComponent();
+        }
+    }
+}
