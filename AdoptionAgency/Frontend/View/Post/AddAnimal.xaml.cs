797ee1bd5ede29a6df27321935ca73f09e6c0f8b
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-07-03 20:17:19 +0200

    feat: implement AddPost View

--- AdoptionAgency/Frontend/View/Post/AddAnimal.xaml.cs
+++ AdoptionAgency/Frontend/View/Post/AddAnimal.xaml.cs
@@ -1,12 +1,22 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace AdoptionAgency.Frontend.View.Post
 {
     public partial class AddAnimal : UserControl
     {
-        public AddAnimal()
+        private readonly Post _parentWindow;
+        public AddAnimal(Post parentWindow)
         {
             InitializeComponent();
+            _parentWindow = parentWindow;
         }
+
+        private void NextPage_Click(object sender, RoutedEventArgs e)
+        {
+            _parentWindow.contentGrid.Children.Clear();
+            _parentWindow.contentGrid.Children.Add(new AddPost());
+        }
+
     }
 }
