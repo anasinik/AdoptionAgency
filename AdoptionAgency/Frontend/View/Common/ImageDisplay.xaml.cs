f1ca587c96c5b49e3ecdd9db47e7577e20af8536
Ana Šinik <sinikana7@gmail.com>
2024-07-03 16:21:50 +0200

    feat: Add ImageDisplay UserControl with PictureViewModel binding

---
+++ AdoptionAgency/Frontend/View/Common/ImageDisplay.xaml.cs
@@ -0,0 +1,29 @@
+﻿using AdoptionAgency.Backend.Domain.Model.Post;
+using AdoptionAgency.Frontend.ViewModel.PostViewModels;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace AdoptionAgency.Frontend.View.Common
+{
+    public partial class ImageDisplay : UserControl
+    {
+        private List<Picture> pictures;
+        public ObservableCollection<PictureViewModel> Pictures { get; set; }
+
+        public ImageDisplay(List<Picture> allPictures)
+        {
+            InitializeComponent();
+            DataContext = this;
+            pictures = allPictures;
+            Pictures = new ObservableCollection<PictureViewModel>();
+            Update();
+        }
+
+        public void Update()
+        {
+            Pictures.Clear();
+            foreach (var picture in pictures)
+                Pictures.Add(new(picture));
+        }
+    }
+}
